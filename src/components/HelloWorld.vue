c9e9346152ab962c890299a7583aee0f7856deee
Tristan <tristan@schoolmouv.fr>
2018-02-08 18:36:08 +0100

    Initial commit for frontend

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,75 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org/en" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>Choice</h1>
+    <div class="tests">
+      <span v-if="tests.length === 0">No test have been created yet :(</span>
+      <div v-for="test in tests" :key="test._id">
+        <span>{{ test.name }}</span>
+        <div class="options">
+          <div v-for="option in test.options" :key="option._id" class="label"
+               :style="{width: option.weight + '%' }">
+            <div>{{ option.label }}</div>
+            <div class="decisionCount">
+              <span>{{ option.decisionCount }}</span>
+              <span :style="{color: (option.decisionCount / test.totalOptionsDecisions * 100
+              < option.weight) ? 'red' : 'green' }" v-if="option.decisionCount > 0">({{
+                (option.decisionCount / test.totalOptionsDecisions * 100
+              - option.weight).toFixed(1)}}%)</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag';
+
 export default {
   name: 'HelloWorld',
+  data: () => ({
+    tests: '',
+  }),
   props: {
     msg: String,
+  },
+  apollo: {
+    tests: gql`{
+      tests{
+        name
+        totalOptionsDecisions
+        options {
+          weight
+          decisionCount
+          label
+        }
+      }
+    }`,
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="stylus" scoped>
+  .tests
+    &>div
+      margin-top 10px
+      .options
+        margin auto
+        width 50%
+        justify-content center
+        font-size 11px
+        display flex
+        .label
+          &>div:first-child
+            cursor pointer
+            transition 0.2s
+            border 1px solid grey
+            margin 5px
+            border-radius 25px
+            padding 5px
+            &:hover
+              background-color rgba(0,0,0,0.3)
+              color white
 </style>
